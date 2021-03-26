c0ddd29cfb493843076a18e1f44b85661a0717a5
Maya Alli <56855612+mayaalli78@users.noreply.github.com>
2021-03-26 02:29:27 -0400

    tables

--- public/script.js
+++ public/script.js
@@ -1,13 +1,39 @@
-async function getData()
+async function populateRestaurants() 
+{
+
+}
+
+async function getDining()
 {
     const request = await fetch("/api/dining");
     const diningData = await request.json();
-    console.log(diningData);
+    return diningData;
 }
+
+async function setBasicData() {
+    localStorage.setItem('myCat', 'Tom');
+}
+
+async function getBasicData() {
+    return localStorage.getItem('myCat');
+}
+
+function setComplexData(data) {
+    localStorage.setItem('data', JSON.stringify(data));
+}
+
 
 async function windowActions()
 {
-    getData();
+    console.log('loaded window');
+    const dining = await getDining();
+    console.table(dining);
+
+setComplexData(dining);
+const storedDining = localStorage.getItem('data');
+const storedDiningData = JSON.parse(storedDining);
+console.log(storedDining);
+console.log(storedDiningData);
 }
 
 window.onload = windowActions;
