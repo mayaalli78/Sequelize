72f83d745c1a2b838c7d355f1dccb0b7a5d606e1
Maya Alli <56855612+mayaalli78@users.noreply.github.com>
2021-03-29 16:13:21 -0400

    changes

--- public/script.js
+++ public/script.js
@@ -11,11 +11,11 @@ async function getDining()
 }
 
 async function setBasicData() {
-    localStorage.setItem('myCat', 'Tom');
+    localStorage.setItem('item', 'item_2');
 }
 
 async function getBasicData() {
-    return localStorage.getItem('myCat');
+    return localStorage.getItem('item');
 }
 
 function setComplexData(data) {
@@ -28,12 +28,15 @@ async function windowActions()
     console.log('loaded window');
     const dining = await getDining();
     console.table(dining);
+    
 
 setComplexData(dining);
 const storedDining = localStorage.getItem('data');
 const storedDiningData = JSON.parse(storedDining);
 console.log(storedDining);
 console.log(storedDiningData);
+
+
 }
 
 window.onload = windowActions;
